0d8aac08682cc72c3907bf9ebbcd6ea1458df60c
Carlotaml21 <carlotaml21@gmail.com>
2025-04-14 14:52:03 +0200

    created package size card with predefined sizes, waiting to add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { ViewPackageSize } from "./pages/ViewPackageSize";
 import { FirstView } from "./pages/FirstView";
 import { ViewDeliveryAcces } from "./pages/ViewDeliveryAcces";
 import { ViewUserAcces } from "./pages/ViewUserAcces";
-import { PackageSizeCard } from "./components/PackageSizeCard";
-import { PackageSizeGroup } from "./components/PackageSizeGroup";
 import { ViewDeliveryMan } from "./pages/ViewDeliveryMan";
 import  { PendingPackages } from "./pages/PendingPackages";
 import "./styles/styles.css";
