c961e93c0ffe37e5e0aa909f7bb116a91286fc40
Rosa <angelica-2025@outlook.es>
2025-04-15 19:07:08 +0200

    WIP: Created InUseSizeLocker modal (incomplete)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import { InUseSizeLocker } from './modals/InUseSizeLocker'
-import { NoAvailableLockers }  from './modals/NoAvailableLockers'
+//import { NoAvailableLockers }  from './modals/NoAvailableLockers'
+import { AllLockersFull } from './modals/AllLockersFull';
 import './styles/styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NoAvailableLockers />
+    <AllLockersFull />
   </StrictMode>,
 )
---
+++ src/modals/AllLockersFull.jsx
@@ -0,0 +1,24 @@
+import "../styles/styles.css";
+import React, { useState } from "react";
+
+export const AllLockersFull = () => {
+    const [isModalOpen, setModalOpen] = useState(false);
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
+    return (                                
+        <div id="modal-container" className="fixed inset-0 flex items-center justify-center">
+            <div id="modal-content" className="bg-[#BEC7FF] p-7 rounded shadow-md w-[40%] text-center">
+                <p className="py-5 text-5xl">❌ </p>
+                <div id="modal-btns-inUseLocker" className="flex flex-row justify-center gap-2">
+                    <button id="btn-choose-size" onClick={openModal} className="w-[36%] bg-[#FFFFFF] text-black py-2 px-4 rounded border font-bree text-center cursor-pointer mb-2">
+                        CANCELAR ENTREGA
+                    </button>
+                </div>
+                <h2 className="text-xm py-1">
+                    *Contacta con el cliente para completar entrega alternativa*
+                </h2>
+            </div>
+        </div>
+    );
+};
