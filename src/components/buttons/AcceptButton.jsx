a2946f7faa53ba38c75f80a7cfe1221b1a85d8c5
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-15 16:42:26 +0200

    fix: styles for accept button

--- src/components/buttons/AcceptButton.jsx
+++ src/components/buttons/AcceptButton.jsx
@@ -2,6 +2,7 @@ import { WhiteButton } from "./WhiteButton";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ErrorCodeModal } from "../modals/ErrorCodeModal";
+import { ExitModalButton } from "./ExitModalButton";
 
 export const AcceptButton = ({ inputCode, text = "Aceptar", redirectPath = "/vistatamaños", ...props }) => {
     //const navigate = useNavigate();
@@ -22,9 +23,10 @@ export const AcceptButton = ({ inputCode, text = "Aceptar", redirectPath = "/vis
                 <ErrorCodeModal onClose={() => setShowModal(false)}>
                     <p className="py-5 text-6xl">❌ </p>
                     <div className="text-center px-4">
-                        <p className="text-[1.75rem] font-medium leading-snug">
+                        <p className="text-[1.75rem] font-medium leading-snug mb-10">
                             Código de entrega incorrecto
                         </p>
+                        <ExitModalButton className="text-center text-sm" text="Cerrar" onClick={() => setShowModal(false)} />
                         </div>
                 </ErrorCodeModal>
             )}
