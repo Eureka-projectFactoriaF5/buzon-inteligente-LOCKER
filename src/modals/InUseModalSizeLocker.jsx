31c2bae44049a4bbbac1b2b45c2b82f5acf69f21
Rosa <angelica-2025@outlook.es>
2025-04-15 10:48:24 +0200

    InUseModalSizeLocker completed

--- src/modals/InUseModalSizeLocker.jsx
+++ src/modals/InUseModalSizeLocker.jsx
@@ -8,17 +8,17 @@ export const InUseModalSizeLocker = () => {
     const closeModal = () => setModalOpen(false);
 
     return (
-        <div id="modal-inUseLocker" className="fixed inset-0 flex items-center justify-center sm:px-6 lg:px-20">
-            <div id="modal-container-inUseLocker" className="bg-[#BEC7FF] p-8 rounded shadow-md w-[40%] text-center">
-                <img id="imagen-error" src="/error.png" alt="Error" className="w-10 h-10 sm:w-12 sm:h-12 mx-auto mb-4"/>
-                <h1 id="tittle-modal" className="text-base sm:text-xl font-semibold mb-6 whitespace-nowrap overflow-hidden text-ellipsis">
+        <div id="modal-container" className="fixed inset-0 flex items-center justify-center sm:px-6 lg:px-20">
+            <div id="modal-content" className="bg-[#BEC7FF] p-8 rounded shadow-md w-[40%] text-center">
+                <p className="py-5 text-6xl">❌ </p>
+                <h1 className="text-base sm:text-xl font-semibold mb-6 whitespace-nowrap overflow-hidden text-ellipsis">
                     Los lockers de ese tamaño están ocupados
                 </h1>
-                <div id="button-hoose-size" className="flex flex-col sm:flex-row justify-center gap-4">
-                    <button id="choose-size-button" onClick={openModal} className="w-[35%] bg-[#FFFFFF] text-black py-2 px-4 rounded border hover:opacity-90 transition font-bree text-center text-sm sm:text-base cursor-pointer">
+                <div id="modal-buttons-inUseLocker" className="flex flex-col sm:flex-row justify-center gap-2">
+                    <button id="button-choose-size" onClick={openModal} className="w-[36%] bg-[#FFFFFF] text-black py-2 px-4 rounded border hover:opacity-90 transition font-bree text-center text-sm sm:text-base cursor-pointer">
                         ESCOGER TAMAÑO
                     </button>
-                    <button id="botton-exit" onClick={closeModal} className="w-[35%] bg-[#00174B] text-white py-2 px-4 rounded border hover:opacity-90 transition font-bree text-center text-sm sm:text-base cursor-pointer">
+                    <button id="botton-exit" onClick={closeModal} className="w-[36%] bg-[#00174B] text-white py-2 px-4 rounded border hover:opacity-90 transition font-bree text-center text-sm sm:text-base cursor-pointer">
                         SALIR
                     </button>
                 </div>
