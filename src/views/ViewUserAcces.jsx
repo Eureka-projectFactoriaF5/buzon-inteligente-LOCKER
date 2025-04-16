5d548af39c219bac52367d50f3321122227f361b
Emagmunioz <emagmunioz@gmail.com>
2025-04-16 09:45:52 +0200

    create EK-93-crear-vista-entrega-repartidor-recogida-usuario

---
+++ src/views/ViewUserAcces.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ViewUserAcces() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#4574FF] to-[#00174B] text-white relative">
+      <div className="flex justify-between items-center mb-8">
+         <img src="/public/IMGS Proyecto/logosinfondoazul.png" alt="Logo Eureka" className="h-10 md:h-20" />
+      </div>
+      </div>)}
