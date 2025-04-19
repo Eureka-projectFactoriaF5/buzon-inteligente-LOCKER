b2edbfd97f188621f039e9ec40eb9944cbaca506
Patricia <bregon@gmail.com>
2025-04-19 12:42:12 +0200

    Update Router.jsx

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -1,20 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-//import { ExampleRoute } from "./routes/ExampleRoute";
-import {PackageDashboardRoute} from "./routes/PackageDashboardRoute";
-import { PackageDetailsRoute } from "./routes/PackageDetailsRoute";
-import { HomeRoute } from "./routes/HomeRoute";
 
-// TODO: Descomenta los campos y rellenalos con una ruta válida
+
 export const router = createBrowserRouter(
     [
         {
             path: "/",
-            children: [HomeRoute],
+            children: [],
         },
         {
-            path: "/packages",
-            children: [PackageDashboardRoute, PackageDetailsRoute]
+            path: "*",
+            element: null,
         },
     ],
     { basename: "/" }
 )
+
