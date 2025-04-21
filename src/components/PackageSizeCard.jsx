077fb2f5d3b07b27e66a8e9c6afaf2bb54d97355
Carlotaml21 <carlotaml21@gmail.com>
2025-04-21 12:54:51 +0200

    add funcionality

--- src/components/PackageSizeCard.jsx
+++ src/components/PackageSizeCard.jsx
@@ -1,25 +1,30 @@
-const PackageSizeCard = ({ size, dimensions, label, sizeClass }) => {
-    return (
-        <div
-        className="bg-white rounded-xl w-58 h-88 p-4 flex flex-col justify-between items-center text-center hover:scale-105 transition-transform"
-        style={{ boxShadow: 'inset -8px 0 10px -6px rgba(0, 0, 0, 0.1)' }}
-      >
-        <div className="flex-1 flex items-center justify-center">
-          <img
-            src="/images/IconoPaquete.svg"
-            alt={`Paquete ${label}`}
-            className={`${sizeClass}`}
-          />
-        </div>
-        <div className="font-bree">
-          <h3 className="font-bold text-2xl text-black -mt-10">{size}</h3>
-          <p className="text-xs text-black">{dimensions}</p>
-          <p className="font-medium mt-1 text-black">{label}</p>
-        </div>
+const PackageSizeCard = ({ size, dimensions, label, sizeClass, isSelected, onSelect }) => {
+  return (
+    <div
+      onClick={() => onSelect(size)}
+      className={`bg-white rounded-xl w-58 h-88 p-4 flex flex-col justify-between items-center text-center 
+        hover:scale-105 transition-transform cursor-pointer 
+        ${isSelected ? 'ring-200 ring-blue-500' : 'shadow-md'}
+      `}
+      style={{ boxShadow: isSelected ? '0 0 0 3px #60a5fa' : 'inset -8px 0 10px -6px rgba(0, 0, 0, 0.1)' }}
+    >
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          src="/images/IconoPaquete.svg"
+          alt={`Paquete ${label}`}
+          className={`${sizeClass}`}
+        />
       </div>
-    );
-  };
-  
-  export default PackageSizeCard;
+      <div className="font-bree">
+        <h3 className="font-bold text-2xl text-black -mt-10">{size}</h3>
+        <p className="text-xs text-black">{dimensions}</p>
+        <p className="font-medium mt-1 text-black">{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PackageSizeCard;
+
   
   
--- src/components/buttons/WhiteButton.jsx
+++ src/components/buttons/WhiteButton.jsx
@@ -1,6 +1,7 @@
-export const WhiteButton = ({ text, type = "button", ...props}) => (
+export const WhiteButton = ({ text, type = "button", disabled = false, ...props}) => (
     <button
       type={type}
+      disabled={disabled}
       {...props}
       className="flex justify-center items-center w-[10.625rem] h-[2.8125rem] bg-[#FCFCFE] text-[#00174B] rounded-[0.625rem] text-[1.125rem] border-none mx-auto cursor-pointer font-bree"
     >
