eb068159f79b71e17ecfe1536cdc65a43f4eb516
karinalorenzo <karinalrg16@gmail.com>
2025-04-21 15:22:24 +0200

    fix: size button

--- src/components/buttons/WhiteButtonModal.jsx
+++ src/components/buttons/WhiteButtonModal.jsx
@@ -2,7 +2,7 @@ export const WhiteButtonModal = ({ text, type = "button", ...props }) => (
   <button
     type={type}
     {...props}
-    className="flex justify-center items-center w-[19.18rem] h-[2.8125rem] bg-[#FCFCFE] text-[#000000] rounded-[0.2rem] text-[1.125rem] border-[0.0625rem] border-black mx-auto cursor-pointer font-inter"
+    className="flex justify-center items-center w-1/2 bg-[#FCFCFE] text-[#000000] rounded-[0.2rem] text-[1.125rem] border-[0.0625rem] border-black mx-auto cursor-pointer font-bree"
   >
     {text}
   </button>
