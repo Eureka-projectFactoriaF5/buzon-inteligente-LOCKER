f0f9b54372571e308eb564f4fb7764f213a92746
PFBregon <bregon@gmail.com>
2025-04-21 15:04:06 +0200

    fixing styles to %

--- src/components/buttons/ExitModalButton.jsx
+++ src/components/buttons/ExitModalButton.jsx
@@ -2,7 +2,7 @@ export const ExitModalButton = ({ text, type = "button", ...props }) => (
     <button
       type={type}
       {...props}
-      className="flex justify-center items-center w-[19.18rem] h-[2.8125rem] bg-[#00174b] text-[#FFFFFF] rounded-[0.2rem] text-[1.125rem] border-[0.0625rem] border-[#00174b] mx-auto cursor-pointer font-inter"
+      className="flex justify-center items-center w-1/2 bg-[#00174b] text-[#FFFFFF] rounded-[0.2rem] text-[1.125rem] border-[0.0625rem] border-[#00174b] mx-auto cursor-pointer font-bree"
     >
       {text}
     </button>
