6cb96f21ace2d086cf953205a53231130a908697
Rosa <angelica-2025@outlook.es>
2025-04-21 13:15:14 +0200

    Files added to modals

--- src/components/modals/NoAvailableLockers.jsx
+++ src/components/modals/NoAvailableLockers.jsx
@@ -1,5 +1,6 @@
 import "../styles/styles.css";
 import React, { useState } from "react";
+import { WhiteButtonModal } from "../buttons/WhiteButtonModal"
 
 export const NoAvailableLockers = () => {
     const [isModalOpen, setModalOpen] = useState(false);
@@ -7,16 +8,14 @@ export const NoAvailableLockers = () => {
     const closeModal = () => setModalOpen(false);
 
     return (                                
-        <div id="modal-container" className="fixed inset-0 flex items-center justify-center">
-            <div id="modal-content" className="bg-[#BEC7FF] p-7 rounded shadow-md w-[40%] text-center">
+        <div id="modal-container" className="fixed inset-0 flex items-center justify-center font-bree">
+            <div id="modal-content" className="bg-[#BEC7FF] p-7 rounded shadow-md w-[60%] text-center">
                 <p className="py-5 text-6xl">❌ </p>
                 <h1 className="text-2xl font-bree mb-4">
                     Todos los lockers están ocupados
                 </h1>
-                <div id="modal-btns-inUseLocker" className="flex flex-row justify-center gap-2">
-                    <button id="btn-choose-size" onClick={openModal} className="w-[36%] bg-[#FFFFFF] text-black py-2 px-4 rounded border font-bree text-center cursor-pointer mb-2">
-                        CANCELAR ENTREGA
-                    </button>
+                <div id="modal-btns-inUseLocker" className="flex flex-row justify-center gap-2 ">
+                    <WhiteButtonModal text="CANCELAR ENTREGA" />      
                 </div>
                 <h2 className="text-xm py-1">
                     *Contacta con el cliente para completar entrega alternativa*
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import { NoAvailableLockers }  from './modals/NoAvailableLockers'
 import './styles/styles.css';
-//import { InUseSizeLocker } from './modals/InUseSizeLocker';
 import { AllLockersFull } from './modals/AllLockersFull';
 
 createRoot(document.getElementById('root')).render(
