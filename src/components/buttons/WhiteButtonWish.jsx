b52f66f7892e98d3cd734048b39083223b5a22d4
karinalorenzo <karinalrg16@gmail.com>
2025-04-22 10:51:55 +0200

    size to button changed

--- src/components/buttons/WhiteButtonWish.jsx
+++ src/components/buttons/WhiteButtonWish.jsx
@@ -2,7 +2,7 @@ export const WhiteButtonWish = ({ text, type = "button", ...props }) => (
     <button
       type={type}
       {...props}
-      className="flex justify-center items-center w-3/4 max-w-[30rem] h-[4rem] bg-[#FCFCFE] text-[#00174B] rounded-[0.375rem] text-[1.125rem] border-none mx-auto cursor-pointer font-bree"
+      className="flex justify-center items-center w-1/4 max-w-[30rem] bg-[#FCFCFE] text-[#00174B] rounded-[0.376rem] text-[1.625rem] border-none mx-auto cursor-pointer font-bree"
     >
       {text}
     </button>
