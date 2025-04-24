0d4a8134e423cb518de20410a32c5cde4fc63800
Carlotaml21 <carlotaml21@gmail.com>
2025-04-24 14:08:43 +0200

    Add last missing view — all views are now complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { ViewPackageSize } from "./pages/ViewPackageSize";
 import { FirstView } from "./pages/FirstView";
 import { ViewDeliveryAcces } from "./pages/ViewDeliveryAcces";
 import { ViewUserAcces } from "./pages/ViewUserAcces";
-import { PackageSizeCard } from "./components/PackageSizeCard";
-import { PackageSizeGroup } from "./components/PackageSizeGroup";
 import { ViewDeliveryMan } from "./pages/ViewDeliveryMan";
+import {ViewUserPickup} from "./pages/ViewUserPickup";
 import  { PendingPackages } from "./pages/PendingPackages";
 import "./styles/styles.css";
 
@@ -17,6 +16,7 @@ export default function App() {
      <ViewPackageSize /> 
       <ViewDeliveryMan />
       <ViewUserAcces />
+      <ViewUserPickup />
       <PendingPackages />
     </>
   );
