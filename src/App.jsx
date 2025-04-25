e1bbf0050a5f54e3aa2f9287459e23d45ab1a362
Emma <emmalanza@hotmail.com>
2025-04-25 07:37:37 +0200

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import React from "react";
-import { ViewPackageSize } from "./pages/ViewPackageSize";
-import { FirstView } from "./pages/FirstView";
-import { ViewDeliveryAcces } from "./pages/ViewDeliveryAcces";
-import { ViewUserAcces } from "./pages/ViewUserAcces";
-import { PackageSizeCard } from "./components/PackageSizeCard";
-import { PackageSizeGroup } from "./components/PackageSizeGroup";
-import { ViewDeliveryMan } from "./pages/ViewDeliveryMan";
-import  { PendingPackages } from "./pages/PendingPackages";
+import AppRouter from "./routing/Router";
 import "./styles/styles.css";
 
 export default function App() {
   return (
-    <>
-     <FirstView />
-     <ViewDeliveryAcces />
-     <ViewPackageSize /> 
-      <ViewDeliveryMan />
-      <ViewUserAcces />
-      <PendingPackages />
-    </>
+    <AppRouter />
   );
 }
 
