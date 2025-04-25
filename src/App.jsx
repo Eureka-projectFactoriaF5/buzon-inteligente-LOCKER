22b65216b5a458a3cd61c21ae266ebe8336f9fd1
Lidia GT <lgtablasf5@gmail.com>
2025-04-25 15:06:29 +0200

    restart code of theapp.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { Keyboard } from './components/keyboard/Keyboard.jsx'
-/* import Keyboard from './components/keyboard/Keyboard.jsx' */
-
-/* import PackageSizeCard from './components/PackageSizeCard'
-import PackageSizeGroup from './components/PackageSizeGroup'
-import ViewPackageSize from './pages/ViewPackageSize' */
-import './styles/styles.css'
+import React from "react";
+import { ViewPackageSize } from "./pages/ViewPackageSize";
+import { FirstView } from "./pages/FirstView";
+import { ViewDeliveryAcces } from "./pages/ViewDeliveryAcces";
+import { ViewUserAcces } from "./pages/ViewUserAcces";
+import { ViewDeliveryMan } from "./pages/ViewDeliveryMan";
+import {ViewUserPickup} from "./pages/ViewUserPickup";
+import  { PendingPackages } from "./pages/PendingPackages";
+import "./styles/styles.css";
 
 export default function App() {
-
   return (
     <>
-      <Keyboard />
+     <FirstView />
+     <ViewDeliveryAcces />
+     <ViewPackageSize /> 
+      <ViewDeliveryMan />
+      <ViewUserAcces />
+      <ViewUserPickup />
+      <PendingPackages />
     </>
-  )
+  );
 }
+
