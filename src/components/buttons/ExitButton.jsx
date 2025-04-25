2e41af36249ea58083664b501e533fbd3359ebfd
PFBregon <bregon@gmail.com>
2025-04-25 14:04:50 +0200

    exitButton funcionality added

--- src/components/buttons/ExitButton.jsx
+++ src/components/buttons/ExitButton.jsx
@@ -1,9 +1,20 @@
-export const ExitButton = ({ text, type = "button", ...props}) => (
+import { useNavigate } from "react-router-dom";
+
+export const ExitButton = ({ text, type = "button", ...props }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/FirstView"); 
+  };
+
+  return (
     <button
       type={type}
       {...props}
+      onClick={handleClick}
       className="flex justify-center items-center w-[11.375rem] h-[4.063rem] bg-[#00328C] text-[#FFFFFF] rounded-[0.625rem] text-[2.625rem] border-none mx-auto cursor-pointer font-bree"
     >
       {text}
     </button>
   );
+};
